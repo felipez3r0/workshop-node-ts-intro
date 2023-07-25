18575c9c53e85ded6d22d68912e49cc250606683
Felipe do E. Santo <felipez3r0@gmail.com>
2023-07-25 13:44:28 -0300

    feat: configura o start do express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('Olá!')
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+  console.log(`Servidor executando na porta ${port}`)
+})
