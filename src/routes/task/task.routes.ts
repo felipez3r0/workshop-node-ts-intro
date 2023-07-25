3f75dcc5ed22a0d7ee778f8d6ba508e4cdf03626
Felipe do E. Santo <felipez3r0@gmail.com>
2023-07-25 15:57:48 -0300

    feat: cria endpoints de listar tasks e buscar task por id

--- src/routes/task/task.routes.ts
+++ src/routes/task/task.routes.ts
@@ -4,5 +4,7 @@ import TaskController from '../../controllers/task/task.controller'
 const taskRoutes = Router()
 
 taskRoutes.post('/', TaskController.store)
+taskRoutes.get('/', TaskController.index)
+taskRoutes.get('/:id', TaskController.show)
 
 export default taskRoutes
